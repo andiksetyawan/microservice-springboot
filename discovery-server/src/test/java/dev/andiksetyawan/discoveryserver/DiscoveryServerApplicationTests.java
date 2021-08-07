aa46b69a48c66824537a54b2f945ecc17e3748d4
Andik Setyawan <andik.setyawan19@gmail.com>
2021-08-07 21:03:59 +0700

    discovery/registry server

---
+++ discovery-server/src/test/java/dev/andiksetyawan/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andiksetyawan.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
